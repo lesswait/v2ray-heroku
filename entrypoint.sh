b19e0c47a6bf44d9bab85e75bcef41d299850ce1
luowei <luowei@udesk.cn>
2021-02-08 10:59:56 +0800

    1

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,12 +26,11 @@ rm -rf v2ray.zip
 #下载最新版caddy
 CADDY_TAG_URL="https://api.github.com/repos/caddyserver/caddy/releases/latest"
 CADDY_VER=`wget -qO- "$CADDY_TAG_URL" | grep 'tag_name' | cut -d\" -f4`
-CADDY_VER="v2.3.0"
-CADDY_VER_NUM="2.3.0"
+CADDY_VER="v1.0.4"
 mkdir /caddybin
 mkdir /caddybin/caddy_$CADDY_VER
 cd /caddybin/caddy_$CADDY_VER
-wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/caddyserver/caddy/releases/download/$CADDY_VER/caddy_$CADDY_VER_NUM$BitVer"
+wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/caddyserver/caddy/releases/download/$CADDY_VER/caddy_$CADDY_VER$BitVer"
 tar xvf caddy.tar.gz
 chmod +x caddy
 rm -rf caddy.tar.gz
